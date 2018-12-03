cf89edb538ae0c610d6edbd85784da7d9f682dd2
sane4 <danyakryloff@gmail.com>
2018-12-03 23:24:25 +0300

    #1

--- main.cpp
+++ main.cpp
@@ -1,55 +1,61 @@
 #include "Note.h"
+const int size1 = 8;//размер
+Note notes[size1];//массив знаков
+int j, n = size1;
+void Sort(int j, int n) {
+	do {
+		
+		int nn = 0;
+		for (j = 1; j < n; ++j)
+			if (notes[j - 1].Day() > notes[j].Day()
+				&& notes[j - 1].Month() > notes[j].Month()
+				&& notes[j - 1].Year() > notes[j].Year()) {
+				Note temp = notes[j - 1];
+				notes[j - 1] = notes[j];
+				notes[j] = temp;
+				nn = j;
+			}
+		n = nn;
+	} while (n);
 
+}
 int main(){
-	const int size = 8;//размер
+	string phone;
 	setlocale(LC_ALL, "");
-	Note notes[size];//массив знаков
 	int choice;
 	while(true){
 		 cout << "\n1. Считать\n2. Поиск\n3. Изменить\n4. Выход\n-> ";
 		 cin >> choice;
-		 if(choice == 1){
+		 switch(choice){
+		 case 1:
 			 cout << "Имя Фамилия Телефон DD MM YY\n";
 			 try{
 				 //считываем данные
-				for(int i = 0; i < size; ++i){
+				for(int i = 0; i < size1; ++i){
 				   cin >> notes[i];
 				}
 				/*Сортируем*/
-				int j, n = size;
-				do {
-					int nn = 0;
-					for (j = 1; j < n; ++j)
-						if (notes[j-1].Day() > notes[j].Day() 
-							&& notes[j-1].Month() > notes[j].Month()
-							&& notes[j-1].Year() > notes[j].Year()) {
-							Note temp = notes[j-1];
-							notes[j-1] = notes[j];
-							notes[j] = temp;
-							nn = j;
-						}
-						n = nn;
-				} while (n);
+				Sort(j, n);
 			 }
 			 catch(const Exception& e){
 				cout << "\n" << e.what();
 			 }
-		 }
-		 else if(choice == 2){
+			 break;
+		 case 2:
 			 cout << "Введите номер телефона: ";
-			 string phone;
+			 
 			 cin >> phone;
 			 int i;
-			 for(i = 0; i < size; ++i){
+			 for(i = 0; i < size1; ++i){
 				if(notes[i].Phone() == phone){
 					cout << "\n" << notes[i];
 				}
 			 }
-			 if(i > phone){
+			 if(i > size1){
 				cout << "NOT FOUND";
 			 }
-		 }
-		 else if(choice == 3){
+			 break;
+		 case 3:
 			 int index;
 			 do{
 				cout << "Введите индекс: ";
@@ -57,9 +63,10 @@ int main(){
 			 } while(index < 0 || index >= 8);//защита от некорректного ввода
 			 cout << "Имя Фамилия Телефон DD MM YY\n";
 			 cin >> notes[index];
-		 }
-		 else if(choice == 4){
+			 break;
+		 case 4:
 			 return 0;
+			 break;
 		 }
 	}
 }
